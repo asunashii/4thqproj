314e253ab9f0207d107971ffb6447df12ddf0c77
ai-one <134765517+asunashii@users.noreply.github.com>
2023-05-27 17:53:22 +0800

    it works

--- js/booking.js
+++ js/booking.js
@@ -1,34 +1,25 @@
-function summaRise() {
-    window.open("htdocs/summary.html"); //does not work, needs debugging
-}
 
-let display = document.getElementById("ticket");
+let display = document.getElementById("summary");
 function travelTicket() {
+
     let name, email, gender, dest, bday, tdate, time, bag;
 
     name = document.getElementById("inputName").value;
     email = document.getElementById("emailAdd").value;
-    gender = document.getElementsByName("gender").value;
-    // dest = document.getElementsByName("gender").value;
     bday = document.getElementById("Birthdate").value;
     tdate = document.getElementById("travelDate").value;
-    time = document.getElementById("time").time;
-    bag = document.getElementsByName("baggage").value;
-
+    time = document.getElementById("time").value;
     gender = document.getElementsByName("gender");
 
     for (i = 0; i < gender.length; i++) {
         if (gender[i].checked) {
             gender = gender[i].value;
-        }
-        else {
-            gender = "Did not select";
+            console.log(gender)
         }
     }
 
     let result = ` Name: ${name} <br/> Email: ${email} <br/> Gender: ${gender} <br/> Birthday: ${bday} 
     <br/> Destination: ${bday} <br/> Date and Time: ${tdate}, ${time} <br/> Baggage check: ${bag} <br/> `;
-
 
     display.innerHTML = result;
 
@@ -38,3 +29,5 @@ function travelTicket() {
 function clearForm() {
     display.innerHTML = "";
 }
+
+
